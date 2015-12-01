18fa5f5b614bdcd89342ff355ceebda2d14168b5
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2015-12-01 04:55:11 -0800

    crate project

---
+++ maptest/maptest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  maptest
+//
+//  Created by Trithep Ratanapipop on 12/1/2558 BE.
+//  Copyright © 2558 Trithep Ratanapipop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ maptest/maptest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  maptest
+//
+//  Created by Trithep Ratanapipop on 12/1/2558 BE.
+//  Copyright © 2558 Trithep Ratanapipop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
