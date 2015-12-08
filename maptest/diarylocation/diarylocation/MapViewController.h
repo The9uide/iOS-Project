f94af52a2d7920f71e4d37ad860f3078b38e088b
Trithep Ratanapipob <the_prince_of_nightmare@hotmail.co.jp>
2015-12-08 14:36:37 +0700

    add new controller

---
+++ maptest/diarylocation/diarylocation/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  diarylocation
+//
+//  Created by Trithep Ratanapipop on 12/8/2558 BE.
+//  Copyright © 2558 Trithep Ratanapipop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ maptest/diarylocation/diarylocation/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  diarylocation
+//
+//  Created by Trithep Ratanapipop on 12/8/2558 BE.
+//  Copyright © 2558 Trithep Ratanapipop. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
